d81b5410c80e675f1d002ab1ee0166d07b474b97
Minafarah200 <141730340+Minafarah200@users.noreply.github.com>
2025-09-08 20:10:03 +0300

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,15 @@
+
+  #include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;   // قراءة العدد N
+
+    for (int i = 1; i <= N; i++) {
+        cout << i << endl;  // طباعة كل رقم في سطر منفصل
+    }
+
+    return 0;
+}
+
